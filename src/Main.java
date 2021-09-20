c8e11fe0830121cf6d65c33c9672bd0a2ce76cca
Hopeless2 <89778966354@mail.ru>
2021-09-20 19:26:00 +0300

    file.delete() fix

--- src/Main.java
+++ src/Main.java
@@ -5,25 +5,25 @@ import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) {
         GameProgress player1 = new GameProgress(15, 2, 3, 3.2);
         GameProgress player2 = new GameProgress(24, 5, 6, 17.5);
         GameProgress playerAdmin = new GameProgress(1337, 1337, 1337, 1337);
 
-        saveGame("C:\\Users\\User\\IdeaProjects\\InstallDota2\\games\\savegames\\player1.dat", player1);
-        saveGame("C:\\Users\\User\\IdeaProjects\\InstallDota2\\games\\savegames\\player2.dat", player2);
-        saveGame("C:\\Users\\User\\IdeaProjects\\InstallDota2\\games\\savegames\\playerAdmin.dat", playerAdmin);
+        saveGame("C:\\Users\\Елисей\\IdeaProjects\\InstallDota2\\games\\savegames\\player1.dat", player1);
+        saveGame("C:\\Users\\Елисей\\IdeaProjects\\InstallDota2\\games\\savegames\\player2.dat", player2);
+        saveGame("C:\\Users\\Елисей\\IdeaProjects\\InstallDota2\\games\\savegames\\playerAdmin.dat", playerAdmin);
 
-        File save1 = new File("C:\\Users\\User\\IdeaProjects\\InstallDota2\\Games\\savegames\\", "player1.dat");
-        File save2 = new File("C:\\Users\\User\\IdeaProjects\\InstallDota2\\Games\\savegames\\", "player2.dat");
-        File save3 = new File("C:\\Users\\User\\IdeaProjects\\InstallDota2\\Games\\savegames\\", "playerAdmin.dat");
+        final File save1 = new File("C:\\Users\\Елисей\\IdeaProjects\\InstallDota2\\Games\\savegames\\", "player1.dat");
+        final File save2 = new File("C:\\Users\\Елисей\\IdeaProjects\\InstallDota2\\Games\\savegames\\", "player2.dat");
+        final File save3 = new File("C:\\Users\\Елисей\\IdeaProjects\\InstallDota2\\Games\\savegames\\", "playerAdmin.dat");
 
         List<File> saveList = new ArrayList<>();
         saveList.add(save1);
         saveList.add(save2);
         saveList.add(save3);
 
-        zipFiles("C:\\Users\\User\\IdeaProjects\\InstallDota2\\Games\\savegames\\zip.zip", saveList);
+        zipFiles("C:\\Users\\Елисей\\IdeaProjects\\InstallDota2\\Games\\savegames\\zip.zip", saveList);
 
         for (File file: saveList){
             removeFile(file);
@@ -46,9 +46,10 @@ public class Main {
     public static void zipFiles(String pathZip, List<File> list) {
         ZipOutputStream zout = null;
         FileInputStream fis = null;
+        FileOutputStream fos = null;
 
         try{
-            zout = new ZipOutputStream(new FileOutputStream(pathZip));
+            zout = new ZipOutputStream(fos = new FileOutputStream(pathZip));
             for (File file : list) {
                 fis = new FileInputStream(file);
                 ZipEntry entry = new ZipEntry(file.getName());
@@ -57,29 +58,42 @@ public class Main {
                 fis.read(buffer);
                 zout.write(buffer);
                 zout.closeEntry();
+                try {
+                    if (fis != null){
+                        fis.close();
+                    }
+                } catch (IOException ex) {
+                    System.out.println(ex.getMessage());
+                }
             }
         }catch (Exception e){
             System.out.println(e.getMessage());
-        }finally {
-            try {
-                if (zout != null) {
-                    zout.close();
-                }
-                if (fis != null){
-                    fis.close();
-                }
-            } catch (IOException ex) {
-                System.out.println(ex.getMessage());
-            }
-
         }
+
+        try {
+            if (zout != null) {
+                zout.close();
+            }
+            if (fos != null){
+                fos.close();
+            }
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+
     }
 
     public static void removeFile(File file) {
-        if (file.delete()) {
-            System.out.println(file.getName() + " удален");
-        }else{
-            System.out.println(file.getName() + " не удален");
+        try{
+            File file2 = new File(file.getCanonicalPath());
+            if (file2.delete()) {
+                System.out.println(file.getName() + " удален");
+            }else{
+                System.out.println(file.getName() + " не удален");
+            }
+        }catch (IOException e){
+            System.out.println(e.getMessage());
         }
+
     }
 }
